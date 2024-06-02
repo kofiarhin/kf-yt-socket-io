395d53e0dc21196372a5ef976db6292f316d7097
kofi <kofiarhin69@gmail.com>
2024-06-02 07:31:37 +0100

    update

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const path = require("path");
 const app = express();
 
 const port = process.env.PORT || 5000;
+const staticPath = path.resolve(__dirname, ".", "dist");
 
 const server = http.createServer(app);
 
@@ -23,5 +25,13 @@ io.on("connection", (socket) => {
     console.log("client disconnected");
   });
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.get("*", (req, res) => {
+    app.use(express.static(staticPath));
+    const indexFile = path.join(__dirname, "dist", "index.html");
+    return res.sendFile(indexFile);
+  });
+}
 
 server.listen(port, () => console.log(`server listening on port ${port}`));
